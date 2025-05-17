cac3f0358d1906990b681cb3be4f96461fe68ad6
Nicolau <17990891+nicxlau@users.noreply.github.com>
2025-05-17 20:03:09 -0300

    adding marshall configs

--- src/dynamodb/client.ts
+++ src/dynamodb/client.ts
@@ -3,7 +3,12 @@ import dynamodbLite from '@aws-lite/dynamodb'
 import AbstractModel from './model'
 
 export const aws = await awsLite({
-  plugins: [dynamodbLite]
+  plugins: [dynamodbLite],
+  // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/Package/-aws-sdk-lib-dynamodb/#configuration
+  // @ts-ignore
+  awsjsonMarshall: {convertClassInstanceToMap: true},
+  // @ts-ignore
+  awsjsonUnmarshall: {convertClassInstanceToMap: true}
 })
 
 export class Dynamodb {
