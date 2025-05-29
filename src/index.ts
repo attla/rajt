860e612b621010201f7722f9ae572f7b6c6a8ab7
Nicolau <17990891+nicxlau@users.noreply.github.com>
2025-05-29 13:05:28 -0300

    adding enum helper

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as Action } from './action'
 export { default as Middleware } from './middleware'
 export { default as JsonResponse } from './response'
+export { default as Enum } from './enum'
