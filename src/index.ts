a11f6998293d2cd7e7ea4a7dd7dd17e52d818748
Nicolau <17990891+nicxlau@users.noreply.github.com>
2025-07-18 12:18:19 -0300

    add alias Gate for auth

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { default as Action } from './action'
-export { Auth } from './auth/auth'
+export { Auth, Auth as Gate } from './auth/auth'
 export { default as Middleware } from './middleware'
 export { default as Request } from './request'
 export { default as Response } from './response'
