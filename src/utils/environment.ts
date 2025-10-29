231d3c7ee74fcef37116e527cf3d8636972f0b1a
Nicolau <17990891+nicxlau@users.noreply.github.com>
2025-10-28 22:27:25 -0300

    improv environment detect

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,19 +1,33 @@
-export default function getEnvironment() {
+const prd = Symbol('prd')
+const dev = Symbol('dev')
+
+let env = prd
+
+export const getEnv = () => env
+export const setEnv = (e: symbol) => env = e
+
+export function detectEnvironment() {
   try {
     if (
-      process.env?.npm_lifecycle_event === 'dev'
+      process.env?.npm_lifecycle_event == 'dev'
       || process.env?.AWS_SAM_LOCAL
-      || process?.argv?.includes('--dev')
+      // || process?.argv?.includes('--dev')
+      || process?.argv?.some(arg => ['--dev', '--development', '--watch'].includes(arg))
       || process?.execArgv?.includes('--watch')
       || import.meta.url?.includes('localhost')
     ) {
-      return 'dev'
+      return dev
     }
   } catch (e) { }
 
-  return 'prod'
+  return prd
 }
 
-export const isDev = () => getEnvironment() === 'dev'
-export const isProd = () => getEnvironment() === 'prod'
+export const isEnv = (e: symbol) => env == e
+
+export const isDev = () => env == dev
+export const isProd = () => env == prd
+
+export const isDevelopment = isDev
+export const isProduction = isProd
 export const isPrd = isProd
